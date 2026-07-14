65739f648c8e33a75c1791bffdfdc2905bec6dce
felipe.freitas <felipe.freitas@foursys.com.br>
2026-07-14 15:34:52 -0300

    Configurando, Banco de dados, Docker e dependencias.

---
+++ LogFel/src/main/java/br/com/felipefreitas/LogFel/LogFelApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipefreitas.LogFel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogFelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogFelApplication.class, args);
+	}
+
+}
---
+++ LogFel/src/test/java/br/com/felipefreitas/LogFel/LogFelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipefreitas.LogFel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogFelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
